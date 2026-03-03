274e4c026ed58edba5613c07f814cedb6146bb08
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2026-03-03 13:59:17 +0100

    refactoring, added conditions, validations

--- src/main/java/cz/ivosahlik/interview/task/historian_hysteria_01/HistorianHysteriaWithoutStream.java
+++ src/main/java/cz/ivosahlik/interview/task/historian_hysteria_01/HistorianHysteriaWithoutStream.java
@@ -8,14 +8,20 @@ public class HistorianHysteriaWithoutStream {
     public static int totalDistance(int[] left, int[] right) {
         long start = System.nanoTime();
 
+        if (left == null || right == null) {
+            throw new IllegalArgumentException("Left or right array is null");
+        }
+
+        if (left.length != right.length) {
+            throw new IllegalArgumentException("Count values in left and right must be equal");
+        }
+
         Arrays.sort(left);
         Arrays.sort(right);
 
-        // int sum = IntStream.range(0, left.length).map(i -> Math.abs(right[i] - left[i])).sum(); // Slower
-
         int sum = 0;
-        for (int i = 0; i < left.length; i++) {
-            sum += Math.abs(right[i] - left[i]);
+        for (int i = 0; i < right.length; i++) {
+            sum += Math.abs(left[i] - right[i]);
         }
 
         System.out.println("Čas: " + (System.nanoTime() - start) / 1_000_000.0 + " ms");
@@ -27,7 +33,7 @@ public class HistorianHysteriaWithoutStream {
         // I took dataset from section `For Example`. There is not attached any file with large dataset like is by description.
 
         int[] left = {3, 4, 2, 1, 3, 3};
-        int[]  right = {4, 3, 5, 3, 9, 3};
+        int[] right = {4, 3, 5, 3, 9, 3};
 
         System.out.println(totalDistance(left, right));
     }
